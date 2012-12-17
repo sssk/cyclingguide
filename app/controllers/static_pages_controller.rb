e161965010d3015ebb20be3801dcd75a0b2625cb
sssk <skinawy@gmail.com>
2012-12-17 10:48:41 -0500

    first commit

---
+++ app/controllers/static_pages_controller.rb
@@ -0,0 +1,11 @@
+class StaticPagesController < ApplicationController
+  def home
+  end
+
+  def about
+  end
+
+  def city_selected
+  end
+
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Cyclingguide::Application.config.secret_token = '3054a2abba190dc65c4fa2fb3d2159ddcaf6e26f4850ee4dcf7a531fc5470098f42da7e44b3151f3daf153a074857b681049fa6f6f82bde3d5bf5b2a0579e291'
